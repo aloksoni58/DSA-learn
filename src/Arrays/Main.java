3d9d8fde2a29ccdf36037fa17eaa8f5f324aec69
alok <alokanilverma80@gmail.com>
2026-05-26 08:32:05 +0530

    git commit -m "problem-1 print pairs in an array"

---
+++ src/Arrays/Main.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2,4,6,8,10};
+        arrayProblems.printPairs(numbers);
+    }
+}
