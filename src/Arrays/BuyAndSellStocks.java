9158fba7ba9b6e141bf1c697a9b86b70456f8416
alok <alokanilverma80@gmail.com>
2026-06-06 09:33:49 +0530

    BuyAndSellStocks

---
+++ src/Arrays/BuyAndSellStocks.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class BuyAndSellStocks {
+	
+	public static void buyAndSellStocks(int[] prices) {
+		
+		int buyPrice = Integer.MAX_VALUE;
+		int maximumProfit = 0;
+		
+		for(int i = 0; i < prices.length; i++) {
+			if(buyPrice < prices[i]) {//profit
+				int profit = prices[i] - buyPrice;//today's profit
+				maximumProfit = Math.max(maximumProfit, profit);
+			}
+			else {
+				buyPrice = prices[i];
+			}
+		}
+		
+		System.out.println("Maximum profit achives is: " + maximumProfit);
+	}
+	
+}
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -2,17 +2,21 @@ package Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        int[] numbers = {-2,-4,-6,-8,-10};
-        arrayProblems.printPairs(numbers);
-        arrayProblems.subArray(numbers);
-        arrayProblems.subarraySum(numbers);
-        arrayProblems.minMaxSubarraySum(numbers);
-        arrayProblems.maxSubarraySumUsingPrefixSum(numbers);
-        arrayProblems.kadanes(numbers);
-    	
-    	int[] arr = {4,2,0,6,3,2,5};
-    	TrappedRainWater.trappedRainWater(arr);
-    }
+		int[] numbers = { -2, -4, -6, -8, -10 };
+		arrayProblems.printPairs(numbers);
+		arrayProblems.subArray(numbers);
+		arrayProblems.subarraySum(numbers);
+		arrayProblems.minMaxSubarraySum(numbers);
+		arrayProblems.maxSubarraySumUsingPrefixSum(numbers);
+		arrayProblems.kadanes(numbers);
+
+		int[] arr = { 4, 2, 0, 6, 3, 2, 5 };
+		TrappedRainWater.trappedRainWater(arr);
+
+		int[] prices = { 7, 1, 5, 3, 6, 4 };
+//		int[] prices = {7,6,5,4,3,1};
+		BuyAndSellStocks.buyAndSellStocks(prices);
+	}
 }
